96694c4afbc8e8ac6daa9e0e9b73d55d740550f2
Vijay <vijaynatarajan@protonmail.com>
2025-02-27 21:25:11 -0500

    added more template stuff

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,33 +3,27 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
 import App from './App.tsx';
-import Board from './pages/Board.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import EditTicket from './pages/EditTicket.tsx';
-import CreateTicket from './pages/CreateTicket.tsx';
-import Login from './pages/Login.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        element: <Board />
+        // element: < /> Login
       }, 
       {
-        path: '/edit',
-        element: <EditTicket />
+        path: '/createUser',
+        // element: < /> CreateUser
       },
       {
-        path: '/create',
-        element: <CreateTicket />
+        path: '/menu',
+        // element: < />  MenuPage
       },
       {
-        path: '/login',
-        element: <Login />
+        path: '/orders',
+        // element: < /> Orders
       }
     ]
   }
