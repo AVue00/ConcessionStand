e31d688ab0c94d25078d969f0e683033f02eed1e
AVue2 <Vuey00@gmail.com>
2025-03-11 20:57:48 -0400

    Updated server.ts routing for render

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import path from 'path';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
 
@@ -9,10 +10,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.use(express.json());
 app.use(routes);
+
+// Catch-all route to serve index.html for client-side routing
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 sequelize.sync({force: false}).then(() => {
   app.listen(PORT, () => {
